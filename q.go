058aeff2090393d6d193337c40c7e4b90bc0133a
Philip Allen <philip.allen@searchdiscovery.com>
2015-05-14 21:02:45 -0400

    initial commit

---
+++ q.go
@@ -0,0 +1,128 @@
+package q
+
+// Tiny abstraction library for consuming/polling AWS SQS messages
+
+import (
+	"errors"
+
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/service/sqs"
+)
+
+const defaultMaxMessages = 10
+const defaultVisibilityTimeout = 1
+const defaultWaitTimeSeconds = 20
+
+var ErrNotFound = errors.New("No messages found")
+
+type Queue struct {
+	endpoint string
+	params   QueueParams
+	sqs      *sqs.SQS
+	poll     bool
+}
+
+type QueueParams struct {
+	MaxMessages       int64
+	VisibilityTimeout int64
+	WaitTimeSeconds   int64
+
+	receiveMessageInput *sqs.ReceiveMessageInput
+}
+
+type Message struct {
+	ID            *string
+	ReceiptHandle *string
+	Body          string
+}
+
+type poller func([]*Message, error)
+
+func (qq *Queue) Receive() (msgs []*Message, err error) {
+	ms, err := qq.sqs.ReceiveMessage(qq.params.receiveMessageInput)
+
+	for _, m := range ms.Messages {
+		msgs = append(msgs, &Message{
+			ID:            m.MessageID,
+			ReceiptHandle: m.ReceiptHandle,
+			Body:          *m.Body,
+		})
+	}
+
+	if len(msgs) == 0 && err == nil {
+		return msgs, ErrNotFound
+	}
+	return msgs, err
+}
+
+func (qq *Queue) Delete(msgs []*Message) error {
+	input := &sqs.DeleteMessageBatchInput{
+		QueueURL: aws.String(qq.endpoint),
+		Entries:  make([]*sqs.DeleteMessageBatchRequestEntry, 0),
+	}
+	for _, msg := range msgs {
+		input.Entries = append(input.Entries, &sqs.DeleteMessageBatchRequestEntry{
+			ID:            msg.ID,
+			ReceiptHandle: msg.ReceiptHandle,
+		})
+	}
+	_, err := qq.sqs.DeleteMessageBatch(input)
+	return err
+}
+
+func (qq *Queue) Shift() ([]*Message, error) {
+	msgs, err := qq.Receive()
+	if err != nil {
+		return msgs, err
+	}
+
+	delErr := qq.Delete(msgs)
+	return msgs, delErr
+}
+
+func (qq *Queue) Poll(p poller) {
+	qq.poll = true
+
+	for {
+		if !qq.poll {
+			break
+		}
+
+		msgs, err := qq.Shift()
+
+		// naïve: the p process could take a long time--don't want these channels
+		// to back up/exhaust our resources.
+		go p(msgs, err)
+	}
+}
+
+func (qq *Queue) StopPoll() {
+	qq.poll = false
+}
+
+func (qp *QueueParams) defaults(endpoint string) {
+	if qp.MaxMessages == 0 {
+		qp.MaxMessages = defaultMaxMessages
+	}
+	if qp.VisibilityTimeout == 0 {
+		qp.VisibilityTimeout = defaultVisibilityTimeout
+	}
+	if qp.WaitTimeSeconds == 0 {
+		qp.WaitTimeSeconds = defaultWaitTimeSeconds
+	}
+
+	qp.receiveMessageInput = &sqs.ReceiveMessageInput{
+		QueueURL:            aws.String(endpoint),
+		MaxNumberOfMessages: aws.Long(qp.MaxMessages),
+		VisibilityTimeout:   aws.Long(qp.VisibilityTimeout),
+		WaitTimeSeconds:     aws.Long(qp.WaitTimeSeconds),
+	}
+}
+
+func New(endpoint string, params QueueParams) *Queue {
+	params.defaults(endpoint)
+	return &Queue{
+		params:   params,
+		endpoint: endpoint,
+	}
+}
---
+++ q_test.go
@@ -0,0 +1,14 @@
+package q_test
+
+import (
+	. "bitbucket.org/searchdiscovery/q"
+
+	. "github.com/onsi/ginkgo"
+	// . "github.com/onsi/gomega"
+)
+
+var _ = Queue{}
+
+var _ = Describe("Q", func() {
+
+})
